10cfd18a6b2b08a3fc17d03d50df99a799592ace
LamZykoss <74079755+LamZykoss@users.noreply.github.com>
2021-03-07 15:55:41 -0300

    Update __init__.py

--- API/v1/br/progress/__init__.py
+++ API/v1/br/progress/__init__.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 async def handler(req):
-    seasonstart="2020-12-02T08:00:00Z"
+    seasonstart="2020-12-02T05:00:00Z"
     seasonend="2021-03-16T08:00:00Z"
 
     daysgone = int((datetime.utcnow().timestamp()-dp.parse(seasonstart).timestamp())/86400)
